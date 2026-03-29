7cdf4e9b87c1c0910ede3a91d3d218f5d68fb500
Mohammed Alfarra <132915634+Moe-alfarra@users.noreply.github.com>
2026-03-29 04:32:51 -0400

    Update api.js

--- Smart-Library-Management System-Frontend/src/services/api.js
+++ Smart-Library-Management System-Frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://elza-glucosidic-johna.ngrok-free.dev/api';
 
 const request = async (endpoint, options = {}) => {
   const token = localStorage.getItem('token');
